a17c7f6ce2e4e571bc45057f98bccee3b0f6a2b1
Amir Khan <mohd.amir@dotsquares.com>
2022-12-06 16:35:19 +0530

    initial commit

---
+++ app/src/main/java/com/amir/norequestlogger/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.amir.norequestlogger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.amir.chuck.RequestLogger
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
